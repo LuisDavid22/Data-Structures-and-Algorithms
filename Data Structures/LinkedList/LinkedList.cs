7bb27dad0dae526d16652454b074b4f5814f63c9
Luis David Laureano Canario <E1008184@ercbpo.com>
2021-04-28 11:28:38 -0400

    Adding linkedList  data structure

---
+++ Data Structures/LinkedList/LinkedList.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAndAlgorithms.Data_Structures
+{
+    class LinkedList
+    {
+        private class Node
+        {
+            public int value { get; set; }
+            public Node next { get; set; }
+        }
+
+        private Node first { get; set; }
+        private Node last { get; set; }
+        private int size { get; set; }
+
+
+       public void addFirst(int item)
+        {
+            if(isEmpty())
+                first = last = new Node { value = item };
+            else
+            {
+                first = new Node { value = item, next = first };
+            }
+
+            size++;        
+        }
+
+        public void addLast(int item)
+        {
+            if (isEmpty())
+                addFirst(item);
+            else
+            {
+                var newLast = new Node { value = item };
+                last.next = newLast;
+                last = newLast;
+                size++;
+            }
+           
+         
+
+            //Node previousLastNode = new Node();
+
+            //if(first.next == null)
+            //{
+            //    previousLastNode = first;
+            //}
+            //else
+            //{
+            //    previousLastNode = first.next;
+            //}
+
+            //while(previousLastNode.next != null)
+            //{
+            //    previousLastNode = previousLastNode.next;
+            //}
+
+            //previousLastNode.next = last;
+        }
+        public void deleteFirst()
+        {
+            if (isEmpty())
+                throw new InvalidOperationException();
+
+            if(first == last)
+                first = last = null;
+            else
+            {
+                var second = first.next;
+                first.next = null;
+                first = second;
+            }
+
+            size--;
+            //var firstNode = first;
+            //first = firstNode.next;
+            //firstNode.next = null;
+        
+        }
+        public void deleteLast()
+        {
+            if (isEmpty())
+                throw new InvalidOperationException();
+
+
+            if (first.next == null)
+                first = last = null;
+            else{
+                var previousLastNode = getPrevious(last);
+
+                previousLastNode.next = null;
+                last = previousLastNode;
+            }
+
+         
+            size--;
+        }
+        private Node getPrevious(Node node)
+        {
+            Node previousLastNode = first;
+
+            while (previousLastNode != null)
+            {
+                if (previousLastNode.next == node)
+                    return previousLastNode;
+
+                previousLastNode = previousLastNode.next;
+            }
+
+            return null;
+        }
+        public bool contains(int value)
+        {
+            return indexOf(value) != -1;
+
+            //return false;
+        }
+        public int indexOf(int item)
+        {
+            Node node = first;
+            int counter = 0;
+
+            while (node != null)
+            {
+                if (node.value == item)
+                    return counter;
+
+                node = node.next;
+                counter++;
+            }
+
+            return -1;
+        }
+        public int Size()
+        {
+            return size;
+        }
+        public int[] toArray()
+        {
+            int[] array = new int[size];
+
+            var current = first;
+            int index = 0;
+            while(current != null)
+            {
+                array[index++] = current.value;
+                current = current.next;
+            }
+
+            return array;
+        }
+        private bool isEmpty()
+        {
+            return first == null;
+        }
+
+        public void revert()
+        {
+            var current = last;
+            Node Previous;
+
+            for (int i = 0; i < size -1; i++)
+            {
+                Previous = getPrevious(current);
+
+                Previous.next = null;
+                
+                current.next = Previous;
+
+                current = current.next;
+            }
+
+            first = last;
+
+            last = current;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DataStructuresAndAlgorithms
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            //testArray();
+            testLinkedList();
+        }
+        private static void testLinkedList()
+        {
+            Data_Structures.LinkedList numbers = new Data_Structures.LinkedList();
+
+            numbers.addLast(1);
+            numbers.addLast(2);
+            numbers.addLast(3);
+            numbers.addLast(4);
+            numbers.addLast(5);
+
+            numbers.addLast(10);
+
+            numbers.deleteFirst();
+
+            numbers.deleteLast();
+
+            Console.WriteLine(numbers.contains(4));
+
+
+            Console.WriteLine(numbers.indexOf(5));
+
+            Console.WriteLine(numbers.Size());
+
+
+            Console.WriteLine(string.Join(',', numbers.toArray()));
+            numbers.revert();
+            Console.WriteLine(string.Join(',', numbers.toArray()));
+
+        }
+        private static void testArray()
         {
             Data_Structures.Array numbers = new Data_Structures.Array(3);
             numbers.insert(10);
