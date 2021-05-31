b80f932d0690de0bd577b39176a9aa45d208b07b
Luis David Laureano Canario <E1008184@ercbpo.com>
2021-05-31 16:03:41 -0400

    Adding tree data structure

---
+++ Data Structures/Trees/Tree.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAndAlgorithms.Data_Structures
+{
+    public class Tree
+    {
+        Node Root;
+        private class Node
+        {
+            public int Value { get; set; }
+            public Node LeftChild { get; set; }
+            public Node RightChild { get; set; }
+        }
+
+        public void insert(int value)
+        {
+            var node = new Node()
+            {
+                Value = value
+            };
+
+
+            if (isEmpty())
+            {
+                Root = node;
+                return;
+            }
+
+            var current = Root;
+
+            while (true)
+            {
+                if (value < current.Value)
+                {
+                    if (current.LeftChild == null)
+                    {
+                        current.LeftChild = node;
+                        break;
+                    }
+                    current = current.LeftChild;
+                }
+                else
+                {
+                    if (current.RightChild == null)
+                    {
+                        current.RightChild = node;
+                        break;
+                    }
+                    current = current.RightChild;
+                }
+            }
+
+        }
+
+        //}
+        //public void insert(int value)
+        //{
+        //    if (isEmpty())
+        //    {
+        //        Root = new Node()
+        //        {
+        //            Value = value
+        //        };
+        //        return;
+        //    }
+
+        //    var current = Root;
+
+        //    while(true)
+        //    {
+        //        if(value > current.Value)
+        //        {
+        //            current = current.RightChild;
+        //        }
+        //        else
+        //        {
+        //            current = current.LeftChild;
+        //        }
+
+        //        if(current == null)
+        //        {
+        //            current = new Node()
+        //            {
+        //                Value = value
+        //            };
+
+        //            break;
+        //        }
+
+        //    }
+
+
+
+        //}
+
+        public bool find(int value)
+        {
+            if (isEmpty())
+                return false;
+
+            var current = Root;
+
+            while(current != null)
+            {
+                if(current.Value == value)
+                {
+                    return true;
+                }
+
+                if (value > current.Value)
+                    current = current.RightChild;
+                else
+                    current = current.LeftChild;
+
+            }
+
+            return false;
+        }
+        public void traverseInOrder()
+        {
+            traverserInOrder(Root);
+        }
+        private void traverserInOrder(Node root)
+        {
+            if (root == null)
+                return;
+
+            traverserInOrder(root.LeftChild);
+            Console.WriteLine(root.Value);
+            traverserInOrder(root.RightChild);
+
+
+        }
+
+        public void traversePostOrder()
+        {
+            traversePostOrder(Root);
+        }
+        private void traversePostOrder(Node root)
+        {
+            if (root == null)
+                return;
+
+            traversePostOrder(root.LeftChild);
+            traversePostOrder(root.RightChild);
+            Console.WriteLine(root.Value);
+            
+
+
+        }
+
+        private bool isEmpty()
+        {
+            return Root == null;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -27,7 +27,24 @@ namespace DataStructuresAndAlgorithms
             //string text = "a green apple";
             //CharFinder finder = new CharFinder();
             //Console.WriteLine(finder.firstRepeatedCharacter(text));
-            testHashTable();
+            //testHashTable();
+            testTree();
+        }
+        public static void testTree()
+        {
+            Tree tree = new Tree();
+
+            tree.insert(7);
+            tree.insert(4);
+            tree.insert(9);
+            tree.insert(1);
+            tree.insert(6);
+            tree.insert(8);
+            tree.insert(10);
+
+            tree.traverseInOrder();
+            Console.WriteLine("\n");
+            tree.traversePostOrder();
         }
         public static void testHashTable()
         {
